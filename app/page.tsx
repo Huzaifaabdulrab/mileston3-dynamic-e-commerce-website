333f53834f16dbe580acad31ac544d30e0c4c3be
huzaifaabdulrab <huzaifaabdulrab2@gmail.com>
2026-04-01 16:45:14 +0500

    Page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import FetchProduct from "./NextComponent/apiFetch";
 import Screen from "./NextComponent/screen";
-
 export default function Home() {
   return(
     <>
+    {/*Screen Section */}
     <Screen/>
+    {/*Fetch Product Section */}
     <FetchProduct/>
-
     </>
   )
 }
